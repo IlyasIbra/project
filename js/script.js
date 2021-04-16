2d9bcc4f323e848a10990085eb7ce9fece29926a
Ilyas <ibradev777@gmail.com>
2021-04-16 23:47:16 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//alert('Hello world!');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = +prompt("You are 18 years old?", "18");
+//console.log(answer + 5);
+
+const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', "");
+//answers[1] = prompt('Как ваш имя?', "");
+//answers[2] = prompt('Как ваше и?', "");
+
+//console.log(typeof(answers));
+//console.log(typeof(null));
+
+//const user = " Ilyas";
+
+//alert(`Privet, ${user}`);
+
+let incr = 10,
+    decr = 10;
+    
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
